097a5cc70a8e76dac0f5710acd5a548d28233201
AmandaCallender <amanda.callender@outlook.com>
2020-01-24 19:27:21 -0500

    JSDinosDone

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -33,19 +33,22 @@ const raptor = {
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(trex.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(raptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stego.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(trex.period);
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+trex.roar = function() {
+  return "RAWERSRARARWERSARARARRRR!";
+};
+console.log(trex.roar());
 
 // ==== Arrays ====
 
